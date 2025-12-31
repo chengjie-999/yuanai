c0ab37ea1d951a53aa9afdc85b2ca51e4902d9eb
24727 <chengjiestipend@163.com>
2025-12-31 10:48:13 +0800

    1

--- spider_lx/ui/selenium_ui.py
+++ spider_lx/ui/selenium_ui.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from spider_lx.auto.web.selenium.selenium_cj import chrome, able_web, app_choose_web
+from spider_lx.auto.web.selenium import selenium_cj
 from spider_lx.parse_data import xiao_yuan
 
 
@@ -9,29 +9,32 @@ def app_main():
     selenium用户界面
     :return:
     """
-    # 唤醒并配置浏览器
-    if st.button('开启浏览器'):
-        if not st.session_state.browser_started:
-            st.session_state.web_driver = chrome()  # 传入实例
-            st.session_state.browser_started = True  # 标记为已启动
-            st.success("浏览器首次启动成功！")
-        else:
-            st.warning("浏览器已启动，无需重复开启！")
+    col1, col2 = st.columns(2)
+    with col1:
+        # 唤醒并配置浏览器
+        if st.button('开启浏览器'):
+            if not st.session_state.browser_started:
+                st.session_state.web_driver = selenium_cj.chrome()  # 传入实例
+                st.session_state.browser_started = True  # 标记为已启动
+                st.success("浏览器首次启动成功！")
+            else:
+                st.warning("浏览器已启动，无需重复开启！")
+    with col2:
+        if st.button('关闭浏览器'):
+            if st.session_state.browser_started:
+                st.session_state.web_driver.quit()
+                st.session_state.web_driver = None
+                st.session_state.browser_started = False
+            else:
+                st.warning("浏览器未启动！")
 
-    if st.button('关闭浏览器'):
-        if st.session_state.browser_started:
-            st.session_state.web_driver.quit()
-            st.session_state.web_driver = None
-            st.session_state.browser_started = False
-        else:
-            st.warning("浏览器未启动！")
-
+    # -------- 步骤1：打开目标网站 --------
     if st.session_state.step == 1 and st.session_state.browser_started:
         driver = st.session_state.web_driver
-        info = able_web()
-        st.session_state.web_code = st.text_input('请选择想要访问的网站编号')
-        if st.button('开启浏览器') and st.session_state.web_code.isdigit():
-            app_choose_web(driver, info, st.session_state.web_code)
+        info = selenium_cj.able_web()
+        st.session_state.web_code = st.selectbox('请选择想要访问的网站', options=info.values())
+        if st.button('打开网站'):
+            selenium_cj.app_choose_web(driver, info, st.session_state.web_code)
 
     # -------- 步骤2：目标网站自动化解析 --------
     if st.session_state.step == 2:
@@ -46,4 +49,8 @@ def app_main():
     # -------- 步骤3：完成（可选） --------
     if st.session_state.step == 3:
         pass
-        # -1. 关闭浏览器
+
+
+if __name__ == '__main__':
+    info = selenium_cj.able_web()
+    print(info.values())
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,27 @@
 import streamlit as st
 from spider_lx.ui import selenium_ui
 
+# =================================== #
+# 运行：streamlit run streamlit_app.py #
+# =================================== #
+
 # ======================
 # 第一步：初始化会话状态（跨步骤保存数据）
 # ======================
 if "web_driver" not in st.session_state:
-    st.session_state.web_driver = ''  # 已完成自动化的网站
+    st.session_state.web_driver = ''  # 浏览器
 
 if "browser_started" not in st.session_state:
     st.session_state.browser_started = False  # 标记浏览器是否已启动
 
-if "web_code" not in st.session_state:
-    st.session_state.web_code = 0  # 已完成自动化的网站
 if "web_name" not in st.session_state:
     st.session_state.web_name = ''  # 已完成自动化的网站
+
 if "step" not in st.session_state:
     st.session_state.step = 1  # 1:输入参数 2:执行爬取 3:完成
 if "url" not in st.session_state:
     st.session_state.url = ""  # 保存用户输入的URL
+
 if "crawl_result" not in st.session_state:
     st.session_state.crawl_result = []  # 保存爬取结果
 

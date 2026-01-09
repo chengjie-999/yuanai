02e378b897964f23e7523ea3580d952b352ab317
24727 <chengjiestipend@163.com>
2026-01-09 09:03:26 +0800

    1

--- spider_lx/ui/selenium_ui.py
+++ spider_lx/ui/selenium_ui.py
@@ -1,8 +1,30 @@
 import streamlit as st
 
 from spider_lx.auto.web.selenium import selenium_cj
-from spider_lx.parse_data import xiao_yuan
 from spider_lx.ui import xiao_yuan_ui
+
+# ======================
+# web自动化首页
+# ======================
+# 第一步：初始化会话状态（跨步骤保存数据）
+# ======================
+if "web_driver" not in st.session_state:
+    st.session_state.web_driver = ''  # 浏览器
+
+if "browser_started" not in st.session_state:
+    st.session_state.browser_started = False  # 标记浏览器是否已启动
+
+if "web_name" not in st.session_state:
+    st.session_state.web_name = ''  # 已完成自动化的网站
+
+if "web_open" not in st.session_state:
+    st.session_state.web_open = False  # 已完成自动化的网站
+
+if "step" not in st.session_state:
+    st.session_state.step = 1  # 1:输入参数 2:执行爬取 3:完成
+
+if "url" not in st.session_state:
+    st.session_state.url = ""  # 保存用户输入的URL
 
 
 def app_main():
@@ -36,10 +58,12 @@ def app_main():
         st.session_state.web_name = st.selectbox('请选择想要访问的网站', options=web_info.values())
         st.write('即将进入：', st.session_state.web_name)
         if st.button('打开网站'):
+            # 打开要访问的网站
             name = selenium_cj.open_web(driver, web_info, st.session_state.web_name)
             if name == st.session_state.web_name and not st.session_state.web_open:
                 st.success('网站已成功打开！')
                 st.session_state.web_open = True
+                # 进入第二步 —— 自动化解析网站
                 st.session_state.step = 2
             else:
                 st.warning('已经有网页打开')
@@ -49,6 +73,8 @@ def app_main():
         try:
             if st.session_state.web_name == '小猿众包':
                 xiao_yuan_ui.main()
+            elif st.session_state.web_name == '':
+                pass
             else:
                 st.write('目标网站自动化待开发~~~')
         except Exception as e:
